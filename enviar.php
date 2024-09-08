6d168fec805cf12ff248fc3ca7b4db83e637cc0b
cesar renteria <153104174+cesaaror@users.noreply.github.com>
2024-09-08 12:23:52 +0200

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -10,18 +10,16 @@ require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recopila los datos del formulario
+    
     $nombre = $_POST["nombre"];
     $email = $_POST["email"];
     $mensaje = $_POST["message"]; // Cambiado a "message"
 
-    // Dirección de correo electrónico a la que enviar el formulario
+   
     $destinatario = "managerjs64@gmail.com";
-
-    // Asunto del correo
+   
     $asunto = "Nuevo mensaje desde el formulario de contacto";
-
-    // Cuerpo del correo
+   
     $cuerpo = "Nombre: $nombre\n";
     $cuerpo .= "Email: $email\n";
     $cuerpo .= "Mensaje:\n$mensaje";
@@ -32,8 +30,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'managerjs64@gmail.com'; // Reemplaza con tu dirección de correo
-        $mail->Password   = 'Danaelsa2'; // Reemplaza con tu contraseña de Gmail
+        $mail->Username   = ''; 
+        $mail->Password   = ''; 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
@@ -50,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo json_encode(["success" => false, "message" => "Error al enviar el mensaje: {$mail->ErrorInfo}"]);
     }
 } else {
-    // Si no es una solicitud POST, redirige a la página principal u otra página de tu elección
+    
     header("Location: index.html");
     exit();
 }
